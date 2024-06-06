cbba44a9cc706846e39a42d79984cf478caee1c3
DanAKASurikata <160882478+DanAKASurikata@users.noreply.github.com>
2024-06-06 15:25:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//main page counting 
+let cislonGL = 0;
+function plusGl() {
+    cislonGL++
+    document.getElementById("cisloGl").innerText = cislonGL;
+    document.getElementById("cisloGl").style.color = 'lightgreen' ;
+}
+function minusGl() {
+    cislonGL--
+    document.getElementById("cisloGl").innerText = cislonGL;
+    document.getElementById("cisloGl").style.color = 'red' ;
+}
+
+//start of the game - hide of main page
+function hideGl() {
+    const elements = document.querySelectorAll('.stranaGlwna');
+    elements.forEach(element => {
+        element.classList.add('hidden');
+    });
+}
+
+
+//
+//
+    //SHOW of some elements
+    //const elements = document.querySelectorAll('.classNazev');
+    //elements.forEach(element => {
+    //    element.classList.remove('hidden');
+    //});
+//
+//
+    //HIDE of some elements
+    //const elements = document.querySelectorAll('.classNazev');
+    //elements.forEach(element => {
+    //    element.classList.add('hidden');
+    //});
+//
+//
